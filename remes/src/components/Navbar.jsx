5640c2f67c1150ddb169f8012b3b1870d2a06dd5
SofiaTissera <sofiatissera@yahoo.com.ar>
2022-08-03 18:15:54 -0300

    menÃú e-commerce

--- remes/src/components/Navbar.jsx
+++ remes/src/components/Navbar.jsx
@@ -4,9 +4,16 @@ import styled from 'styled-components'
 function Navbar() {
   return (
     <>
-        <Navbar>
-            <h2>Navbar <span>Responsive</span></h2>
-        </Navbar>
+        <NavContainer>
+            <h2>Tienda de <span>Remeras</span></h2>
+            <div>
+              <a href="/">Home</a>
+              <a href="/">Shop</a>
+              <a href="/">About</a>
+              <a href="/">Contact</a>
+              <a href="/">Gallery</a>
+            </div>
+        </NavContainer>
     </>
   )
 }
@@ -14,5 +21,29 @@ function Navbar() {
 export default Navbar 
 
 const NavContainer = styled.nav`
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+  }
+h2 {
+  color: white;
+  font-weight: 400;
+  margin-left: 1rem;
+  span{
+    font-weight: bold;
+  }
+}
+padding: .4rem;
+background-color: #836C73;
+display: flex;
+align-items: center;
+justify-content: space-between;
+a{
+  color: white;
+  text-decoration: none;
+  margin-right: 2rem;
+}
 
 `
